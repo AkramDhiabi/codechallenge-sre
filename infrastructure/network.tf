378a56738fd94bcd835e4bf008c982a45305fab9
akram.dhiabi <akram.dhiabi@filestage.io>
2022-02-12 16:11:03 +0100

    add gitignore

---
+++ infrastructure/network.tf
@@ -0,0 +1,154 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-vpc" })
+  )
+}
+
+# Private internet access
+
+resource "aws_eip" "public" {
+  vpc   = true
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-public" })
+  )
+}
+resource "aws_nat_gateway" "public" {
+  # assign public eip to the NAT gateway
+  allocation_id = aws_eip.public.id
+  subnet_id     = aws_subnet.public_a.id
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-public" })
+  )
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-private" })
+  )
+}
+
+resource "aws_route" "private_internet_access" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.public.id
+}
+
+# Public internet access
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-main" })
+  )
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-public" })
+  )
+}
+
+resource "aws_main_route_table_association" "public" {
+  vpc_id         = aws_vpc.main.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+####################################################
+# Public Subnets - Inbound/Outbound internet access #
+
+## Public Subnet a
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              =  "10.0.0.0/19" 
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_region.current.name}a"
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-public-a" })
+  )
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+## Public Subnet b
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.32.0/19"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_region.current.name}b"
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-public-b" })
+  )
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+##############################################
+# Private Subnets - Outbound internet access #
+
+## Private Subnet a
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.64.0/19"
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-private-a" })
+  )
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private.id
+}
+
+## Private Subnet b
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.96.0/19"
+  availability_zone = "${data.aws_region.current.name}b"
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-private-b" })
+  )
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private.id
+}
