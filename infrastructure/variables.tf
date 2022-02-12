d5ff41282a5a76e49db6bf7b9e70da17c32c0605
akram.dhiabi <akram.dhiabi@filestage.io>
2022-02-12 17:23:30 +0100

    add required variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "contact" {
   default = "dhiabi.akram@gmail.com"
 }
+
+variable "auth_image" {
+  description = "ECR Image for auth service"
+}
+
+variable "booking_image" {
+  description = "ECR Image for booking service"
+}
